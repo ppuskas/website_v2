03aa840d4f4e16b1a0e790e3a7da56d0ccc95c6b
ppuskas <ppuskas@gmail.com>
2023-12-06 10:53:55 -0800

    node stuff

---
+++ src/index.js
@@ -0,0 +1,35 @@
+
+import * as THREE from 'three';
+
+// Create a scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('canvas').appendChild(renderer.domElement);
+
+// Create a cube
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+
+// Add the cube to the scene
+scene.add(cube);
+
+// Position the camera
+camera.position.z = 5;
+
+// Create the animation loop
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the cube
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    // Render the scene
+    renderer.render(scene, camera);
+}
+
+animate();
